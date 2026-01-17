5bc091cf7b7817456179d8c203a6bf701d36f329
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2026-01-17 15:19:57 +0800

    Update install-mysql-grafana.sh

--- QuantGo/install-mysql-grafana.sh
+++ QuantGo/install-mysql-grafana.sh
@@ -1,6 +1,23 @@
 
 ### 
-apt install mysql-client-core-8.0
+apt install mysql-client-core-8.0 -y
+
+mkdir -p /home/mysql8/init
+
+cat > /home/mysql8/init/01-create-db.sql << EOF
+CREATE DATABASE IF NOT EXISTS wqb 
+DEFAULT CHARACTER SET utf8mb4 
+DEFAULT COLLATE utf8mb4_unicode_ci;
+EOF
+
+# 赋予可读权限
+chmod 644 /home/mysql8/init/01-create-db.sql
+
+cp wqb.sql /home/mysql8/init/02-import-wqb.sql
+
+sed -i '1i USE wqb;' /home/mysql8/init/02-import-wqb.sql
+
+chmod 644 /home/mysql8/init/02-import-wqb.sql
 
 docker run  -d  \
 --name mysql8 \
@@ -10,9 +27,11 @@ docker run  -d  \
 -v /home/mysql8/data:/var/lib/mysql \
 -v /home/mysql8/config:/etc/mysql/conf.d  \
 -v /home/mysql8/logs:/logs \
+-v /home/mysql8/init:/docker-entrypoint-initdb.d \
 -e MYSQL_ROOT_PASSWORD=123456 \
 -e TZ=Asia/Shanghai mysql \
 --lower_case_table_names=1
+
 
 
 ###
