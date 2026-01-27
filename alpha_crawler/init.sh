cfe5b642bf8a33b179168308a14a6f5f0d422ab1
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2026-01-28 00:39:04 +0800

    Create init.sh

---
+++ alpha_crawler/init.sh
@@ -0,0 +1,32 @@
+### 
+apt install mysql-client-core-8.0 -y
+
+mkdir -p /home/mysql8/init
+
+cat > /home/mysql8/init/01-create-db.sql << EOF
+CREATE DATABASE IF NOT EXISTS consultant_analytics 
+DEFAULT CHARACTER SET utf8mb4 
+DEFAULT COLLATE utf8mb4_unicode_ci;
+EOF
+
+# 赋予可读权限
+chmod 644 /home/mysql8/init/01-create-db.sql
+
+cp database.sql /home/mysql8/init/02-import-wqb.sql
+
+sed -i '1i USE consultant_analytics;' /home/mysql8/init/02-import-wqb.sql
+
+chmod 644 /home/mysql8/init/02-import-wqb.sql
+
+docker run  -d  \
+--name mysql8 \
+--privileged=true \
+--restart=always \
+-p 3310:3306 \
+-v /home/mysql8/data:/var/lib/mysql \
+-v /home/mysql8/config:/etc/mysql/conf.d  \
+-v /home/mysql8/logs:/logs \
+-v /home/mysql8/init:/docker-entrypoint-initdb.d \
+-e MYSQL_ROOT_PASSWORD=123456 \
+-e TZ=Asia/Shanghai mysql \
+--lower_case_table_names=1
